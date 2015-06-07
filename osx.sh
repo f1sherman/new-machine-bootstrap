deaf4ca630b6a9583f0a1db27706fdcf1a5ef647
Brian John <brian@brianjohn.com>
2015-06-06 20:33:58 -0400

    check and add instructions for FileVault

--- osx.sh
+++ osx.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
+instructions=""
+
 function install_if_not_exists {
   if ! command -v $2 >/dev/null 2>&1; then
-    echo "Installing $1"
-    echo $3
+    echo -e "Installing $1\n"
     eval $3
   fi
 }
 
+function add_instruction {
+  instructions="$instructions\n$1"
+}
+
 install_if_not_exists Homebrew brew 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+
+if [ "$(fdesetup status)" != "FileVault is On." ]; then
+  add_instruction 'Setup FileVault'
+fi
+
+if [ "$instructions" != "" ]; then
+  echo -e ""
+  echo -e "---------------------------------"
+  echo -e "| Stuff you need to do manually |"
+  echo -e "---------------------------------"
+  echo -e "$instructions"
+  echo -e ""
+fi
