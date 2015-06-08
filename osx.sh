11f2736dc7f40b68e56c8bbed5de32ccebb12446
Brian John <brian@brianjohn.com>
2015-06-07 20:36:17 -0400

    clone vundle into the proper directory

--- osx.sh
+++ osx.sh
@@ -88,7 +88,7 @@ if [ ! -d ~/.vim ]; then
   ln -s ~/.vim/vimrc ~/.vimrc
   ln -s ~/.vim/gvimrc ~/.gvimrc
   mkdir ~/.vimtmp
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   vim +BundleInstall +qall
   echo -e "\nvim setup complete!\n"
 else
