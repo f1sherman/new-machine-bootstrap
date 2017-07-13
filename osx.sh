52bfb621894bdbd3da8807b666166f2de930fcf7
Brian John <brian@brianjohn.com>
2017-07-13 08:52:10 -0500

    install eslint

--- osx.sh
+++ osx.sh
@@ -136,6 +136,14 @@ brew cleanup
 
 # END BREW RECIPES
 
+# INSTALL eslint
+
+if ! is_binary_installed eslint; then
+  run_with_progress "Installing eslint", "npm install --global eslint"
+fi
+
+# END INSTALL eslint
+
 # GENERATE SSH KEY
 
 if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
